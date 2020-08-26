a3400a5c93ce65aac36d9c356e52dac4fcb9afc5
Sameer Mahajan <sam241099@gmail.com>
2020-08-26 11:48:45 +0530

    chat url fix

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import React from 'react';
 import './App.css';
 import Header from './Header.js';
 import Sidebar from './Sidebar.js';
+import Chat from './Chat';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-   
+      <Router>
       <Header />
       <div className="app__body">
         <Sidebar />
+
+        <Switch>
+          <Route path="/room/:roomId">
+            <Chat />
+          </Route>
+          <Route path="/" >
+            {/* <Chat /> */}
+            <h1>Welcome</h1>
+          </Route>
+
+        </Switch>
+
       </div>
+
+      </Router>
+      
     </div>
   );
 }
